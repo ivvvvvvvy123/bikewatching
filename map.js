5daf28cc7a8d27e7800bb8356e036fadd0301aae
Yixuan Xin <153150068+ivvvvvvvy123@users.noreply.github.com>
2025-05-14 16:41:11 -0700

    step 2 adding bike lane of 2 places

--- map.js
+++ map.js
@@ -15,3 +15,43 @@ const map = new mapboxgl.Map({
   minZoom: 5, // Minimum allowed zoom
   maxZoom: 18, // Maximum allowed zoom
 });
+
+map.on('load', async () => {
+    //code
+    console.log("Map loaded.");
+    //boston bike lanes
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+        });
+        
+    map.addLayer({
+        id: 'boston-bike-lanes',
+        type: 'line',
+        source: 'boston_route',
+        paint: {
+            'line-color': 'green',
+            'line-width': 4,
+            'line-opacity': 0.5,
+        },
+        });
+    console.log("bike lane layers visualization");
+    //cambridge bike lanes
+    map.addSource('cambridge_route', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
+        
+        });
+    map.addLayer({
+        id: 'cambridge-bike-lanes',
+        type: 'line',
+        source: 'cambridge_route',
+        paint: {
+            'line-color': 'green',
+            'line-width': 4,
+            'line-opacity': 0.4,
+        },
+        });
+        console.log("Cambridge bike lanes added to the map.");
+        
+  });
